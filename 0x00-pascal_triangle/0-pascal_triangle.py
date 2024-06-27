a8e30ae58323d9ea044c792cab40cbb01ede4c85
AbdallahM19 <abdallahmaabakar@gmail.com>
2024-06-26 18:35:16 -0700

    interview

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""pascal triangle"""
+
+
+def pascal_triangle(n):
+    if n <= 0 or type(n) is not int:
+        return []
+    triangle = []
+    for i in range(n):
+        r = []
+        for x in range(i + 1):
+            if x == 0 or x == i:
+                r.append(1)
+            else:
+                r.append(triangle[i - 1][x - 1] + triangle[i - 1][x])
+        triangle.append(r)
+    return triangle
