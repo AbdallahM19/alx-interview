74ceef43697ba84a1a39a5ecd004a088770de97c
AbdallahM19 <abdallahmaabakar@gmail.com>
2024-07-04 18:28:48 -0700

    lockboxes

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""python"""
+
+
+def canUnlockAll(boxes):
+    """canUnlockAll function"""
+    n = len(boxes)
+
+    opened_boxes = []
+    opened_boxes.append(0)
+    keys = list(boxes[0])
+
+    while keys:
+        key = keys.pop()
+        if key < n and key not in opened_boxes:
+            opened_boxes.append(key)
+            keys.extend(boxes[key])
+
+    return len(opened_boxes) == n
