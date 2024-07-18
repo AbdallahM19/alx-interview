91e9ac5836939fda3a938fbd89dfad7e82a136ac
AbdallahM19 <abdallahmaabakar@gmail.com>
2024-07-18 16:15:46 -0700

    Log parsing

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -8,44 +8,44 @@ import sys
 import re
 
 
-def print_stats(dict_stats: dict) -> None:
-    """Prints the stats of the dict_stats dictionary"""
-    print("File size: {}".format(dict_stats["total_size"]))
-    for status_frequency in sorted(dict_stats["status_file"]):
-        if dict_stats["status_file"][status_frequency]:
-            print("{}: {}".format(
-                status_frequency, dict_stats["status_file"][status_frequency]
-            ))
+def output(log: dict) -> None:
+    """
+    Output the log
+    """
+    print("File size: {}".format(log["total_size"]))
+    for code in sorted(log["code_frequency"]):
+        if log["code_frequency"][code]:
+            print("{}: {}".format(code, log["code_frequency"][code]))
 
 
 if __name__ == "__main__":
     regex = re.compile(
-        r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3} - \[\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}.\d+\] "GET /projects/260 HTTP/1.1" (.{3}) (\d+)'  # nopep8
-    )
+    r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3} - \[\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}.\d+\] "GET /projects/260 HTTP/1.1" (.{3}) (\d+)')  # nopep8
 
     line_count = 0
-    dict_stats = {}
-    dict_stats["total_size"] = 0
-    dict_stats["status_file"] = {
-        str(status): 0 for status in [
-            200, 301, 400, 401, 403, 404, 405, 500]
-    }
+    log = {}
+    log["total_size"] = 0
+    log["code_frequency"] = {
+        str(code): 0 for code in [
+            200, 301, 400, 401, 403, 404, 405, 500]}
 
     try:
         for line in sys.stdin:
             line = line.strip()
             match = regex.fullmatch(line)
-
-            if match:
+            if (match):
                 line_count += 1
-                status = match.group(1)
+                code = match.group(1)
                 file_size = int(match.group(2))
-                dict_stats["total_size"] += file_size
 
-                if status.isdecimal():
-                    dict_stats["status_file"][status] += 1
+                # File size
+                log["total_size"] += file_size
 
-                if line_count == 10:
-                    print_stats(dict_stats)
+                # status code
+                if (code.isdecimal()):
+                    log["code_frequency"][code] += 1
+
+                if (line_count % 10 == 0):
+                    output(log)
     finally:
-        print_stats(dict_stats)
+        output(log)
