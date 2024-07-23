867a85601c4cfea1716bb41e29ed352e0572d80a
AbdallahM19 <abdallahmaabakar@gmail.com>
2024-07-23 01:39:13 -0700

    Task 0 (DONE) ✔

---
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""UTF-8 Validation"""
+
+
+def validUTF8(data):
+    """
+    a method that determines if a given data
+    set represents a valid UTF-8 encoding.
+    """
+    num_bytes_to_follow = 0
+
+    for i in data:
+        if num_bytes_to_follow > 0:
+            if (i >> 6) != 0b10:
+                return False
+            num_bytes_to_follow -= 1
+        else:
+            if (i >> 7) == 0:
+                num_bytes_to_follow = 0
+            elif (i >> 5) == 0b110:
+                num_bytes_to_follow = 1
+            elif (i >> 4) == 0b1110:
+                num_bytes_to_follow = 2
+            elif (i >> 3) == 0b11110:
+                num_bytes_to_follow = 3
+            else:
+                return False
+
+    return num_bytes_to_follow == 0
