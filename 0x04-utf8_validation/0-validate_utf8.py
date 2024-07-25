c02f702fcc39a7e3e85d312529c6662a59ed922e
AbdallahM19 <abdallahmaabakar@gmail.com>
2024-07-25 04:02:47 -0700

    Task 0 (DONE) ✔

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -16,8 +16,6 @@ def validUTF8(data):
             continue
 
         # Check if data[i] is a valid integer within the UTF-8 range
-        if not (0 <= data[i] <= 0x10ffff) and type(data[i]) != int:
-            return False
 
         # 1-byte character (ASCII)
         if data[i] <= 0x7f:
