01c20f11ec170b2568842263d7068eba9aa7feb4
AbdallahM19 <abdallahmaabakar@gmail.com>
2024-07-25 03:23:28 -0700

    Task 0 (DONE) ✔

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -19,6 +19,8 @@ def validUTF8(data):
                 num_bytes_to_follow = 1
             elif (i >> 7):
                 return False
+            else:
+                return False
         else:
             if (i >> 6) != 0b10:
                 return False
