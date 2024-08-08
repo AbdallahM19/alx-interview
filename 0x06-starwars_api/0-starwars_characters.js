d8f4a68ec636f9acef3b76e04b02380cc693fabe
AbdallahM19 <abdallahmaabakar@gmail.com>
2024-08-08 04:28:40 -0700

    Task

---
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -0,0 +1,29 @@
+#!/usr/bin/node
+
+const request = require('request');
+const movieId = process.argv[2];
+const url = `https://swapi-api.alx-tools.com/api/films/${movieId}/`;
+
+if (movieId && process.argv.length > 2) {
+  request(url, (error, response, body) => {
+    if (error) {
+      console.log(error);
+      return
+    }
+
+    const movie = JSON.parse(body);
+    const characters = movie.characters;
+
+    characters.forEach(characterUrl => {
+      request(characterUrl, (err, res, body) => {
+        if (err) {
+          console.error(err);
+          return;
+        }
+
+        const character = JSON.parse(body);
+        console.log(character.name);
+      });
+    });
+  });
+}
