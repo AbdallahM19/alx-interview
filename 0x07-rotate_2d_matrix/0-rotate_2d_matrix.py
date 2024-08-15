dc827abbbce7abdc64041f1aee1a0ce6ff363b43
AbdallahM19 <abdallahmaabakar@gmail.com>
2024-08-14 20:15:26 -0700

    Task is Done

---
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""solve Rotate 2D Matrix"""
+
+def rotate_2d_matrix(matrix):
+    """rotate_2d_matrix function"""
+    matrix.reverse()
+    for i in range(len(matrix)):
+        for x in range(i + 1, len(matrix)):
+            matrix[i][x], matrix[x][i] = matrix[x][i], matrix[i][x]
