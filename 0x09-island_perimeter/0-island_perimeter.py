8a1d199885ee3ad2e472c4a0758cdc10622da562
AbdallahM19 <abdallahmaabakar@gmail.com>
2024-08-29 02:14:06 -0700

    Island Perimeter

---
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Island Perimeter"""
+
+
+def island_perimeter(grid):
+    """returns the perimeter of the island described in grid"""
+    perimeter = 0
+    rows, cols = len(grid), len(grid[0])
+
+    for i in range(rows):
+        for j in range(cols):
+            if grid[i][j] == 1:
+                if i == 0 or grid[i - 1][j] == 0:  # Check top
+                    perimeter += 1
+                if i == rows - 1 or grid[i + 1][j] == 0:  # Check bottom
+                    perimeter += 1
+                if j == 0 or grid[i][j - 1] == 0:  # Check left
+                    perimeter += 1                
+                if j == cols - 1 or grid[i][j + 1] == 0:  # Check right
+                    perimeter += 1
+    return perimeter
